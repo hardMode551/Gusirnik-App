d3ad3c918c2477712e863acecb8f5713b6c61c41
Sergey Ivanov <yeezusc322@gmail.com>
2023-04-13 20:04:37 +0300

    fix

---
+++ src/Components/Spinner/Spinner.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Spinner.module.scss';
+
+const Spinner: React.FC = () => {
+  return (
+    <div className={styles.ldsRoller}>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Spinner;
--- src/Components/Variants.tsx
+++ src/Components/Variants.tsx
@@ -14,100 +14,115 @@ import gusirnikInMorning from '../assets/gusirnik-in-morning.png';
 import GusBlock from './GusBlock';
 
 import styles from '../stylessheet/Variants.module.scss';
+import Spinner from './Spinner/Spinner';
 
 const Variants: React.FC = () => {
+  const [fake, setFake] = React.useState(false);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setFake(true);
+    }, 2000);
+  }, [fake]);
+
   return (
     <section>
-      <div className={styles.title}>
-        <h1>ГУСЫРНИК!!!!!</h1>
-        <img src={gusirnik} alt="" />
-      </div>
-      <h1>ОН МОЖЕТ БЫТЬ РАЗНЫМ</h1>
-      <GusBlock title="Например таким:">
-        <motion.div
-          initial={{ x: 100, opacity: 0 }}
-          whileInView={{ x: 0, opacity: 1 }}
-          transition={{ duration: 1.4 }}
-        >
-          <img src={gusirnikOnWater} alt="gus" />
-        </motion.div>
-      </GusBlock>
+      {!fake ? (
+        <Spinner />
+      ) : (
+        <div>
+          <div className={styles.title}>
+            <h1>ГУСЫРНИК!!!!!</h1>
+            <img src={gusirnik} alt="" />
+            <h1>ОН МОЖЕТ БЫТЬ РАЗНЫМ</h1>
+          </div>
+          <GusBlock title="Например таким:">
+            <motion.div
+              initial={{ x: 100, opacity: 0 }}
+              whileInView={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1.4 }}
+            >
+              <img src={gusirnikOnWater} alt="gus" />
+            </motion.div>
+          </GusBlock>
 
-      <GusBlock title="Или таким:">
-        <motion.div
-          initial={{ opacity: 0, y: -100 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1.5 }}
-          viewport={{ once: false }}
-        >
-          <img src={gusirnikInCity} alt="gus" />
-        </motion.div>
-      </GusBlock>
+          <GusBlock title="Или таким:">
+            <motion.div
+              initial={{ opacity: 0, y: -100 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1.5 }}
+              viewport={{ once: false }}
+            >
+              <img src={gusirnikInCity} alt="gus" />
+            </motion.div>
+          </GusBlock>
 
-      <GusBlock title="Либо таким:">
-        <motion.div
-          initial={{ x: -100, opacity: 0 }}
-          whileInView={{ x: 0, opacity: 1 }}
-          transition={{ duration: 1.5 }}
-          viewport={{ once: false }}
-        >
-          <img src={gusirnikInArk} alt="gus" />
-        </motion.div>
-      </GusBlock>
+          <GusBlock title="Либо таким:">
+            <motion.div
+              initial={{ x: -100, opacity: 0 }}
+              whileInView={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1.5 }}
+              viewport={{ once: false }}
+            >
+              <img src={gusirnikInArk} alt="gus" />
+            </motion.div>
+          </GusBlock>
 
-      <GusBlock title="Зимним:">
-        <motion.div
-          initial={{ rotate: -100, y: 200, opacity: 0 }}
-          whileInView={{ rotate: 0, y: 0, opacity: 1 }}
-          transition={{ duration: 1.5 }}
-          viewport={{ once: false }}
-        >
-          <img src={gusirnikInWinter} alt="gus" />
-        </motion.div>
-      </GusBlock>
+          <GusBlock title="Зимним:">
+            <motion.div
+              initial={{ rotate: -100, y: 200, opacity: 0 }}
+              whileInView={{ rotate: 0, y: 0, opacity: 1 }}
+              transition={{ duration: 1.5 }}
+              viewport={{ once: false }}
+            >
+              <img src={gusirnikInWinter} alt="gus" />
+            </motion.div>
+          </GusBlock>
 
-      <GusBlock title="Летним:">
-        <motion.div
-          initial={{ scale: 0.5, opacity: 0 }}
-          whileInView={{ scale: 1, opacity: 1 }}
-          transition={{ duration: 1.5 }}
-          viewport={{ once: false }}
-        >
-          <img src={gusirnikInSummer} alt="gus" />
-        </motion.div>
-      </GusBlock>
+          <GusBlock title="Летним:">
+            <motion.div
+              initial={{ scale: 0.5, opacity: 0 }}
+              whileInView={{ scale: 1, opacity: 1 }}
+              transition={{ duration: 1.5 }}
+              viewport={{ once: false }}
+            >
+              <img src={gusirnikInSummer} alt="gus" />
+            </motion.div>
+          </GusBlock>
 
-      <GusBlock title="Вечерним:">
-        <motion.div
-          initial={{ rotate: 180, opacity: 0 }}
-          whileInView={{ rotate: 0, opacity: 1 }}
-          transition={{ duration: 1.5 }}
-          viewport={{ once: false }}
-        >
-          <img src={gusirnikInEvening} alt="gus" />
-        </motion.div>
-      </GusBlock>
+          <GusBlock title="Вечерним:">
+            <motion.div
+              initial={{ rotate: 180, opacity: 0 }}
+              whileInView={{ rotate: 0, opacity: 1 }}
+              transition={{ duration: 1.5 }}
+              viewport={{ once: false }}
+            >
+              <img src={gusirnikInEvening} alt="gus" />
+            </motion.div>
+          </GusBlock>
 
-      <GusBlock title="Или утренним:">
-        <motion.div
-          initial={{ y: 100, opacity: 0 }}
-          whileInView={{ y: 0, opacity: 1 }}
-          transition={{ duration: 1.5 }}
-        >
-          <img src={gusirnikInMorning} alt="gus" />
-        </motion.div>
-      </GusBlock>
+          <GusBlock title="Или утренним:">
+            <motion.div
+              initial={{ y: 100, opacity: 0 }}
+              whileInView={{ y: 0, opacity: 1 }}
+              transition={{ duration: 1.5 }}
+            >
+              <img src={gusirnikInMorning} alt="gus" />
+            </motion.div>
+          </GusBlock>
 
-      <h3>
-        Выбери своего <b>ГУСЫРНИКА</b> и следуй за мечтой!
-      </h3>
-      <img
-        src="https://media.tenor.com/YWHLhjz3QtIAAAAd/%D0%B3%D1%83%D1%81%D1%8C%D1%84%D0%BB%D0%B5%D0%BA%D1%81%D0%B8%D1%82-%D0%B3%D1%83%D1%81%D1%8C.gif"
-        alt="DanceGus"
-        style={{
-          width: '50%',
-        }}
-      />
+          <h3>
+            Выбери своего <b>ГУСЫРНИКА</b> и следуй за мечтой!
+          </h3>
+          <img
+            src="https://media.tenor.com/YWHLhjz3QtIAAAAd/%D0%B3%D1%83%D1%81%D1%8C%D1%84%D0%BB%D0%B5%D0%BA%D1%81%D0%B8%D1%82-%D0%B3%D1%83%D1%81%D1%8C.gif"
+            alt="DanceGus"
+            style={{
+              width: '50%',
+            }}
+          />
+        </div>
+      )}
     </section>
   );
 };
